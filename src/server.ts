64ce1cadca8b9ae4b9ffb7233e26baaf10ee9ba4
azran4u <azran4u@gmail.com>
2019-06-12 09:54:43 +0300

    axios config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,52 @@
-import express from 'express';
-const app = express();
+import axios, {
+  AxiosAdapter,
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  Cancel,
+  Canceler,
+  CancelToken,
+  CancelTokenSource,
+} from 'axios';
 
-app.get('/', (req, res) => res.send('Hello World!'));
+import https from 'https';
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const config: AxiosRequestConfig = {
+  url: '/planetary/apod',
+  // tslint:disable-next-line:object-literal-sort-keys
+  method: 'get',
+  baseURL: 'https://api.nasa.gov',
+  headers: { },
+  params: { api_key: 'ubNbUudM9ENMr3HGpp3c2oDXKkjvpc54TaqztIrf' },
+  timeout: 10000,
+  withCredentials: false,
+  responseType: 'json',
+  maxContentLength: 20000,
+  validateStatus: (status: number) => status >= 200 && status < 300,
+  maxRedirects: 5,
+};
+
+const handleResponse = (response: AxiosResponse) => {
+  console.log('OK');
+  console.log(response.data);
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log(response.headers);
+  console.log(response.config);
+};
+
+const handleError = (error: AxiosError) => {
+  if (error.response) {
+    console.log('ERROR');
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else {
+    console.log(error.message);
+  }
+};
+
+axios(config)
+  .then(handleResponse)
+  .catch(handleError);
