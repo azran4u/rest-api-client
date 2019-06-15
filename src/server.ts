d96ebce4ad4deae7f9de1dd348372b634fd4e9aa
azran4u <azran4u@gmail.com>
2019-06-15 23:57:30 +0300

    created api module

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import * as axios from 'axios';
-import {fetchAPOD} from './api';
+import * as api from './api';
 
 async function handleRequestAPOD() {
   const handleResponse = (response: axios.AxiosResponse) => {
-    console.log('OK');
     console.log(response.data);
     console.log(response.status);
     console.log(response.statusText);
@@ -13,7 +12,6 @@ async function handleRequestAPOD() {
 
   const handleError = (error: axios.AxiosError) => {
     if (error.response) {
-      console.log('ERROR');
       console.log(error.response.data);
       console.log(error.response.status);
       console.log(error.response.headers);
@@ -22,7 +20,7 @@ async function handleRequestAPOD() {
     }
   };
 
-  await fetchAPOD()
+  await api.fetchAPOD()
     .then(handleResponse)
     .catch(handleError);
 
